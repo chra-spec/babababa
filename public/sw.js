536bc34ac67490185e0d43d1eb5e64f45aa76b7a
chra-spec <tncwn4641@gmail.com>
2026-08-22 01:57:14 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -2,8 +2,8 @@ self.addEventListener('push', event => {
   const data = event.data.json();
   const options = {
     body: data.body || 'Yılan seni özledi gel ve skorunu arttır!',
-    icon: '/icon.png',
-    badge: '/icon.png',
+    icon: 'https://img.remit.ee/api/file/BQACAgUAAyEGAASHRsPbAAEZnjVqiNcypAY9RPOqIiCfIwkYE0S-EQACHiEAAgK8SFT-tAhBtaDTQT0E.png',
+    badge: 'https://img.remit.ee/api/file/BQACAgUAAyEGAASHRsPbAAEZnjVqiNcypAY9RPOqIiCfIwkYE0S-EQACHiEAAgK8SFT-tAhBtaDTQT0E.png',
     vibrate: [100, 50, 100],
     lang: 'tr'
   };
